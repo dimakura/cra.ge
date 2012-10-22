b1537a5ba4c2b16ca3082cef5aa8807b409aefc4
Dimitri Kurashvili <dimitri@c12.ge>
2012-10-22 19:01:36 +0400

    getting DOCUMENTS by name and DOB

--- lib/cra/services.rb
+++ lib/cra/services.rb
@@ -15,6 +15,27 @@ module CRA
         http.headers['SOAPAction'] = soap_action
         soap.body = { 'privateNumber' => personal_number }
       end
+      CRA::PasportInfo.init_with_hash(response.to_hash[:get_data_using_private_number_response][:get_data_using_private_number_result])
+    end
+
+    def by_name_and_dob(last_name, first_name, year, month, day)
+      raise ArgumentError('first_name required') if first_name.blank?
+      raise ArgumentError('last_name required') if last_name.blank?
+      raise ArgumentError('year required') if year.blank?
+      raise ArgumentError('month required') if month.blank?
+      raise ArgumentError('day required') if day.blank?
+      action_name = 'GetDataUsingCriteria'
+      soap_action = self.soap_action(action_name)
+      response = get_client.request action_name do
+        http.headers['SOAPAction'] = soap_action
+        soap.body = { 'lastName' => last_name, 'firstName' => first_name, 'year' => year, 'month' => month, 'day' => day }
+      end
+      data = response.to_hash[:get_data_using_criteria_response][:get_data_using_criteria_result][:person_info]
+      documents = []
+      data.each do |row|
+        documents << CRA::PasportInfo.init_with_hash(row)
+      end
+      documents
     end
 
   end
