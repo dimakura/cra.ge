a91b7afe947e7cb180ac4550f0a0da96790e5964
Dimitri Kurashvili <dimitri@c12.ge>
2012-12-04 11:48:21 +0400

    brief documentation for services

--- lib/cra/services.rb
+++ lib/cra/services.rb
@@ -11,6 +11,7 @@ module CRA
 
     public
 
+    # Returns array of passports.
     def by_name_and_dob(lname, fname, year, month, day)
       body = self.gov_talk_request({
         service: 'GetDataUsingCriteriaParameter',
@@ -27,6 +28,7 @@ module CRA
       CRA::PassportInfo.list_with_hash(body)
     end
 
+    # Returns ID card information.
     def by_id_card(private_number, id_card_serial, id_card_numb)
       body = self.gov_talk_request({
         service: 'GetDataUsingPrivateNumberAndIdCardParameter',
@@ -41,6 +43,7 @@ module CRA
       CRA::PassportInfo.init_with_hash(body)
     end
 
+    # Returns passport information.
     def by_passport(private_number, passport)
       body = self.gov_talk_request({
         service: 'FetchPersonInfoByPassportNumberUsingCriteriaParameter',
@@ -54,6 +57,7 @@ module CRA
       CRA::PassportInfo.init_with_hash(body)
     end
 
+    # Returns array of addresses (ids, names and activity).
     def address_by_name(parent_id, name)
       body = self.gov_talk_request({
         service: 'AddrFindeAddressByNameParameter',
